ed6ab85c5c55a7f284242d6ee86b458443174de3
Mike Micatka <mmicatka@datto.com>
2016-08-04 15:39:29 -0400

    working on speed

--- src/talking_data.py
+++ src/talking_data.py
@@ -48,33 +48,53 @@ def get_raw_train_data():
     return train_data
 
 
-def get_installed_histogram_for_device(device_id):
-    events = read_or_load_raw_file('../data/events.csv')
-    # make this easier to use
-    events = events.drop(['timestamp', 'longitude', 'latitude'], axis=1)
-    app_events = read_or_load_raw_file('../data/app_events.csv')
-    app_labels = read_or_load_raw_file('../data/app_labels.csv')
-    label_categories = read_or_load_raw_file('../data/label_categories.csv')
-    events_for_device = events[events['device_id'] == device_id].drop(['device_id'], axis=1)
-    app_events = app_events.drop(['is_installed', 'is_active'], axis=1)
-    apps_for_device = app_events.merge(events_for_device, on='event_id')
-    apps_for_device = apps_for_device.drop('event_id', axis=1)
-    # I think I need to do this to get the unique apps
-    # There is potentially more data in here but I don't know how to use it effectively
-    # change this later in order to get a usage histogram
-    # for now just knowing what is installed is fine
-    apps_for_device = apps_for_device.drop_duplicates()
-    labels_for_device = app_labels.merge(apps_for_device, on='app_id').drop('app_id', axis=1).drop_duplicates(
-        'label_id')
-    hist = label_categories['label_id'].isin(labels_for_device['label_id']).astype(int).to_frame().drop('label_id', axis=1).transpose()
-    hist['device_id'] = device_id
-    return hist
+# def get_installed_histogram_for_device(device_id):
+#     events = read_or_load_raw_file('../data/events.csv')
+#     # make this easier to use
+#     events = events.drop(['timestamp', 'longitude', 'latitude'], axis=1)
+#     app_events = read_or_load_raw_file('../data/app_events.csv')
+#     app_labels = read_or_load_raw_file('../data/app_labels.csv')
+#     label_categories = read_or_load_raw_file('../data/label_categories.csv')
+#     events_for_device = events[events['device_id'] == device_id].drop(['device_id'], axis=1)
+#     app_events = app_events.drop(['is_installed', 'is_active'], axis=1)
+#     apps_for_device = app_events.merge(events_for_device, on='event_id')
+#     apps_for_device = apps_for_device.drop('event_id', axis=1)
+#     # I think I need to do this to get the unique apps
+#     # There is potentially more data in here but I don't know how to use it effectively
+#     # change this later in order to get a usage histogram
+#     # for now just knowing what is installed is fine
+#     apps_for_device = apps_for_device.drop_duplicates()
+#     labels_for_device = app_labels.merge(apps_for_device, on='app_id').drop('app_id', axis=1).drop_duplicates(
+#         'label_id')
+#     hist = label_categories['label_id'].isin(labels_for_device['label_id']).astype(int).to_frame().drop('label_id', axis=1).transpose()
+#     hist['device_id'] = device_id
+#     return hist
+#
+#
+# def get_installed_histograms(device_ids):
+#     histograms = pd.DataFrame(device_ids)
+#     for device_id in device_ids:
+#         histograms = histograms.append(get_installed_histogram_for_device(device_id))
+#     return histograms
 
 
 def get_installed_histograms(device_ids):
     histograms = pd.DataFrame(device_ids)
+    events = read_or_load_raw_file('../data/events.csv')
+    events = events.drop(['timestamp', 'longitude', 'latitude'], axis=1)
+    app_events = read_or_load_raw_file('../data/app_events.csv')
+    app_labels = read_or_load_raw_file('../data/app_labels.csv')
+    label_categories = read_or_load_raw_file('../data/label_categories.csv')
     for device_id in device_ids:
-        histograms = histograms.append(get_installed_histogram_for_device(device_id))
+        events_for_device = events[events['device_id'] == device_id].drop(['device_id'], axis=1)
+        apps_for_device = app_events.merge(events_for_device, on='event_id')
+        apps_for_device = apps_for_device.drop('event_id', axis=1)
+        apps_for_device = apps_for_device.drop_duplicates()
+        labels_for_device = app_labels.merge(apps_for_device, on='app_id').drop('app_id', axis=1).drop_duplicates(
+            'label_id')
+        hist = label_categories['label_id'].isin(labels_for_device['label_id']).astype(int).to_frame().drop('label_id', axis=1).transpose()
+        hist['device_id'] = device_id
+        histograms = histograms.append(hist)
     return histograms
 
 
@@ -82,7 +102,7 @@ def get_processed_train_data():
     raw_train_data = get_raw_train_data()
     phone_brand_model = read_or_load_raw_file('../data/phone_brand_device_model.csv')
     # Temporary for testing
-    processed_train_data = raw_train_data.head(20)
+    processed_train_data = raw_train_data.head(50)
     processed_train_data = pd.merge(processed_train_data, phone_brand_model, on=['device_id'])
     # Breaking this apart for clarity
     hists = get_installed_histograms(processed_train_data['device_id'])
